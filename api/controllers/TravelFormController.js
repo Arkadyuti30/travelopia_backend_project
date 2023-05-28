f0061fd62ab93f80b00aa24028eaaf0445d154bc
arkadyuti30 <arkadyutipaul@gmail.com>
2023-05-29 04:15:42 +0530

    created Travel form model, controller + submit form api + updated routes

---
+++ api/controllers/TravelFormController.js
@@ -0,0 +1,39 @@
+/**
+ * TravelFormController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+  submitForm: (req, res) => {
+    if (!req.body) {
+        sails.log.error(`Error in TravelFormController.submitForm. Missing request body! Body ${req.body}`)
+        return res.badRequest()
+    }
+
+    /***** Sample Body *****/
+    /* 
+        { 
+            name: "Jacob", 
+            email: "jacob123@gmail.com",
+            place: "India",
+            travellers: 3,
+            budget: "3000-4000"
+        }
+    */
+
+    const formData = req.body
+    formData.date = new Date()
+
+    TravelForm.submitForm(formData, (err) => {
+        if (err) {
+            sails.log.error(`Error submitting form data for email ${req.body.email}`)
+            return res.negotiate(err)
+        }
+        return res.ok()
+    })
+  }
+};
+
---
+++ api/models/TravelForm.js
@@ -0,0 +1,61 @@
+/**
+ * TravelForm.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'string',
+      required: true
+    },
+    place: {
+      type: 'string',
+      required: true
+    },
+    travellers: {
+      type: 'number',
+      required: true
+    },
+    budget: {
+      type: 'string',
+      required: true
+    }
+  },
+
+  submitForm: (formData, cb) => {
+    /* 
+      Sample Form Data
+      { 
+        name: "Jacob", 
+        email: "jacob123@gmail.com",
+        place: "India",
+        travellers: 3,
+        budget: "3000-4000"
+      }
+    */
+
+    if (!formData) {
+      sails.log.error(`No form data found! Form data: ${formData}`)
+      return cb({
+        status: 400
+      })
+    }
+    TravelForm.create(formData).fetch().exec((err) => {
+      if (err) {
+        sails.log.error(`Error storing form data ${JSON.stringify(formData)}`)
+        return cb(err)
+      }
+      return cb(null)
+    }
+  } 
+
+};
+
